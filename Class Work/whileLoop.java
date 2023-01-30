7c7beb6acf651b2b03ff0f0f288a37309755e503
Akshitha chinthakindi <89307990+Akshithachinthakindi@users.noreply.github.com>
2023-01-30 20:39:45 +0530

    Add files via upload

---
+++ Class Work/whileLoop.java
@@ -0,0 +1,13 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        //declaration
+        // while(<condition>){
+        // //run this block of code
+        // Incremental step
+        // }
+        int i = 5;
+        while (i<10) {
+            System.out.println(i);
+        }
+    }
+}
