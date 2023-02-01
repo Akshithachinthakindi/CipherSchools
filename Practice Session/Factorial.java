d4f2a55bffea94c4e50568e96b7a37bb1cf547b7
Akshitha <chinthakindiakshitha@gmail.com>
2023-02-01 11:10:30 +0530

    updated

--- Practice Session/Factorial.java
+++ Practice Session/Factorial.java
@@ -1,29 +1,36 @@
+import java.util.Scanner;
+
 public class Factorial {
-    static int factorial (int n) {
-        if (n==0) {
-            return 1;
-        }
-        return n*factorial(n-1);
-    }
     public static void main(String[] args) {
-            // TODO Auto-generated method stub
-            int arr[] = new int[] {5,6,7,8,9};
-            for (int i=0; i<arr.length; i++) {
-                System.out.println(factorial(arr[i]) + " ");
+        int number;
+        long factorial = 1;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number: ");
+        number = input.nextInt();
+        input.close();
+        if (number < 0) {
+            System.out.println("Can't find the factorial of negative number");
+        } else if (number <=1) {
+            System.out.printf("%d! = %d", number, factorial);
+        } else {
+            for (int counter = number; counter >= 2; counter--) {
+                factorial = factorial * counter;
             }
+            System.out.printf("%d! = %d", number, factorial);
+        }
     }
 }
-// ques : Print the factorial of numbers in a range
-// relation between the input and output
-// 1 to 5 range factorials
-// 1 2 6 24 120
-//        Scanner input = new Scanner(System.in);
-//        System.out.print("Enter your first number: ");
-//        int rangeStart = input.nextInt();
-//        System.out.print("Enter your second number: ");
-//        int rangeEnd = input.nextInt();
-//        int fact =1;
-//        for (int i=rangeStart; i<=rangeEnd; i++) {
-//            fact = fact*i;
-//            System.out.println("The Factorial of " + i +" is: " + fact);
+
+//    static int factorial (int n) {
+//        if (n==0) {
+//            return 1;
 //        }
+//        return n*factorial(n-1);
+//    }
+//    public static void main(String[] args) {
+//            // TODO Auto-generated method stub
+//            int arr[] = new int[] {5,6,7,8,9};
+//            for (int i=0; i<arr.length; i++) {
+//                System.out.println(factorial(arr[i]) + " ");
+//            }
+//    }
